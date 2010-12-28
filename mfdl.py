68de2408e56e4b40ae2e0c0db49f1dcb44bd3fab
Kunal Sarkhel <theninja@bluedevs.net>
2010-12-27 23:39:35 -0500

    Fixed downloading of mature mangas and mangas listed in the latest chapters sections. Also cleaned up code for 100% on pep8.py and 10.00/10 on Pylint.

--- mfdl.py
+++ mfdl.py
@@ -2,13 +2,17 @@
 
 '''Mangafox Download Script by Kunal Sarkhel <theninja@bluedevs.net>'''
 
-import sys, os, urllib
+import sys
+import os
+import urllib
 from BeautifulSoup import BeautifulSoup
+#from IPython.Shell import IPShellEmbed #for debug purposes
 
-url_base = "http://mangafox.com/"
+URL_BASE = "http://mangafox.com/"
+#ipshell = IPShellEmbed()
 
 def get_page_soup(url):
-    """Downlaod a page and return a BeautifulSoup object of the html"""
+    """Download a page and return a BeautifulSoup object of the html"""
     urllib.urlretrieve(url, "page.html")
     html = ""
     with open("page.html") as html_file:
@@ -16,17 +20,20 @@ def get_page_soup(url):
             html += line
     return BeautifulSoup(html)
 
+
 def get_chapter_urls(manga_name):
     """Get the chapter list for a manga"""
-    url = url_base + "manga/" + manga_name.lower()
+    url = "{0}manga/{1}?no_warning=1".format(URL_BASE, manga_name.lower())
     soup = get_page_soup(url)
     chapters = []
     links = soup.findAll('a', {"class": "chico"})
     for link in links:
         chapters.append(link['href'])
-    return chapters
+    return list(set(chapters)) # ugly yo-yo code to remove duplicates
+
 
 def get_page_numbers(soup):
+    """Return the list of page numbers from the parsed page"""
     raw = soup.findAll('select', {'class': 'middle'})[0]
     raw_options = raw.findAll('option')
     pages = []
@@ -34,10 +41,11 @@ def get_page_numbers(soup):
         pages.append(html['value'])
     return pages
 
+
 def get_chapter_image_urls(url_fragment):
     """Find all image urls of a chapter and return them"""
     print "Getting chapter urls"
-    chapter_url = url_base + url_fragment
+    chapter_url = URL_BASE + url_fragment
     chapter = get_page_soup(chapter_url)
     pages = get_page_numbers(chapter)
     image_urls = []
@@ -45,24 +53,28 @@ def get_chapter_image_urls(url_fragment):
     for page in pages:
         print "Getting image url from {0}{1}.html".format(url_fragment, page)
         page_soup = get_page_soup(chapter_url + page + ".html")
-        images =  page_soup.findAll('img', {'id': 'image'})
+        images = page_soup.findAll('img', {'id': 'image'})
         image_urls.append(images[0]['src'])
     return image_urls
+
+
+def get_chapter_number(url_fragment):
+    """Parse the url fragment and return the chapter number."""
+    return ''.join(url_fragment.rsplit("/")[3:-1])
+
 
 def download_urls(image_urls, manga_name, chapter_number):
     """Download all images from a list"""
     num = 1
-    try:
-        os.makedirs("{0}/{1}/".format(manga_name, chapter_number))
-    except:
-        None
+    os.makedirs("{0}/{1}/".format(manga_name, chapter_number))
     for url in image_urls:
         filename = "./{0}/{1}/{2:03}.jpg".format(manga_name,
                                                  chapter_number,
                                                  num)
         print "Downloading {0} to {1}".format(url, filename)
         urllib.urlretrieve(url, filename)
-        num = num + 1;
+        num = num + 1
+
 
 def download_manga_range(manga_name, range_start, range_end):
     """Download a range of a chapters"""
@@ -70,13 +82,13 @@ def download_manga_range(manga_name, range_start, range_end):
     chapter_urls = get_chapter_urls(manga_name)
     chapter_urls.sort()
     for url_fragment in chapter_urls[int(range_start)-1:int(range_end)+1]:
-        chapter_number = url_fragment.rsplit("/")[4][1:]
+        chapter_number = get_chapter_number(url_fragment)
         print("===============================================")
         print("Chapter " + chapter_number)
         print("===============================================")
         #image_urls = get_chapter_image_urls(url_fragment)
-        #download_urls(image_urls, manga_name, chapter_number)
     os.remove("page.html")
+
 
 def download_manga(manga_name, chapter_number=None):
     """Download all chapters of a manga"""
@@ -84,7 +96,7 @@ def download_manga(manga_name, chapter_number=None):
     chapter_urls.sort()
     if chapter_number:
         url_fragment = chapter_urls[int(chapter_number)-1]
-        chapter_number = url_fragment.rsplit("/")[4][1:]
+        chapter_number = get_chapter_number(url_fragment)
         print("===============================================")
         print("Chapter " + chapter_number)
         print("===============================================")
@@ -92,7 +104,8 @@ def download_manga(manga_name, chapter_number=None):
         download_urls(image_urls, manga_name, chapter_number)
     else:
         for url_fragment in chapter_urls:
-            chapter_number = url_fragment.rsplit("/")[4][1:]
+            chapter_number = get_chapter_number(url_fragment)
+            print chapter_number
             print("===============================================")
             print("Chapter " + chapter_number)
             print("===============================================")
