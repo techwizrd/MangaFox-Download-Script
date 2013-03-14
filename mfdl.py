8a274f85ae3c2b5bd5fd872ece4c11a615c96326
Kunal Sarkhel <theninja@bluedevs.net>
2013-03-14 18:04:47 -0400

    Fix bug where manga names with spaces were not recognized properly

--- mfdl.py
+++ mfdl.py
@@ -28,6 +28,7 @@ def get_page_soup(url):
 
 def get_chapter_urls(manga_name):
     """Get the chapter list for a manga"""
+    manga_name = manga_name.replace(" ", "_")
     url = "{0}manga/{1}?no_warning=1".format(URL_BASE, manga_name.lower())
     print "Url: " + url
     soup = get_page_soup(url)
